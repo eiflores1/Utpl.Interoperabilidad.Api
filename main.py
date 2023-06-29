5bfad5ffb60b738a6b0de93a9bba9576e3b32c0a
Eduardo Flores <eduflores98@outlook.es>
2023-06-29 02:22:21 +0000

    guardar

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from typing import List, Optional
 import spotipy
 import uuid
 
+from fastapi_versioning import VersionedFastAPI, version
+
 #Importar libreria de mongodb
 import pymongo
 
@@ -65,6 +67,7 @@ class PersonaRepositorio (BaseModel):
     id: str
     nombre: str
     edad: int
+    identificacion: Optional[str] = None
     ciudad: Optional[str] = None
 
 class PersonaEntrada (BaseModel):
@@ -72,22 +75,38 @@ class PersonaEntrada (BaseModel):
     edad: int
     ciudad: Optional[str] = None
 
+class PersonaEntradav2 (BaseModel):
+    nombre: str
+    edad: int
+    identificacion: str
+    ciudad: Optional[str] = None
+
 
 personaList = []
 
 @app.post("/personas", response_model=PersonaRepositorio, tags = ["personas"])
+@version(1, 0)
 async def crear_persona(person: PersonaEntrada):
     itemPersona = PersonaRepositorio(id=str(uuid.uuid4()), nombre= person.nombre, ciudad = person.ciudad, edad = person.edad)
     result = coleccion.insert_one(itemPersona.dict())
     return itemPersona
 
+@app.post("/personas", response_model=PersonaRepositorio, tags = ["personas"])
+@version(2, 0)
+async def crear_personav2(person: PersonaEntradav2):
+    itemPersona = PersonaRepositorio(id=str(uuid.uuid4()), nombre= person.nombre, ciudad = person.ciudad, edad = person.edad, identificacion = person.identificacion)
+    result = coleccion.insert_one(itemPersona.dict())
+    return itemPersona
+
 @app.get("/personas", response_model=List[PersonaRepositorio], tags=["personas"])
+@version(1, 0)
 def get_personas():
     items = list(coleccion.find())
     print (items)
     return items
 
 @app.get("/personas/{persona_id}", response_model=PersonaRepositorio , tags=["personas"])
+@version(1, 0)
 def obtener_persona (persona_id: str):
     item = coleccion.find_one({"id": persona_id})
     if item:
@@ -97,6 +116,7 @@ def obtener_persona (persona_id: str):
     
 
 @app.delete("/personas/{persona_id}", tags=["personas"])
+@version(1, 0)
 def eliminar_persona (persona_id: str):    
     result = coleccion.delete_one({"id": persona_id})
     if result.deleted_count == 1:
@@ -105,11 +125,13 @@ def eliminar_persona (persona_id: str):
         raise HTTPException(status_code=404, detail="Persona no encontrada")
 
 @app.get("/pista/{pista_id}", tags = ["canciones"])
+@version(1, 0)
 async def obtener_pista(pista_id: str):
     track = sp.track(pista_id)
     return track
     
 @app.get("/artistas/{artista_id}", tags = ["canciones"])
+@version(1, 0)
 async def get_artista(artista_id: str):
     artista = sp.artist(artista_id)
     return artista
@@ -118,3 +140,6 @@ async def get_artista(artista_id: str):
 @app.get("/")
 def read_root():
     return {"Hello": "Interoperabilidad 8"}
+
+##encapsular
+app = VersionedFastAPI(app)
